c8bc91b623e0f1768a1f095b4521200788bd0358
Somanath Nemilidinne <114342573+SomanathNemilidinne@users.noreply.github.com>
2024-10-30 17:31:31 +0530

    Create Charactercounter.java

---
+++ Charactercounter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Charactercounter {
+    public static void main(String[] args) {
+        Scanner object = new Scanner(System.in);
+        System.out.println("Give the input: ");
+        String user = object.nextLine();
+        System.out.println("Which character you want to count?: ");
+        char input = object.next().charAt(0);
+        System.out.println("No.of times '" + input + "' has repeated is " + counter(user, input));
+    }
+    public static int counter(String word, char what) {
+        int count = 0;
+        String letter = word;
+        for (char havala : letter.toCharArray()) {
+            if (havala == what) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
