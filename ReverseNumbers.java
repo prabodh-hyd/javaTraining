dce50dd06d0486eb42447cd8d95d0285157bd49e
Somanath Nemilidinne <114342573+SomanathNemilidinne@users.noreply.github.com>
2024-11-02 15:15:02 +0530

    Update ReverseNumbers.java

--- ReverseNumbers.java
+++ ReverseNumbers.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Main {
+class ReverseNumbers {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         System.out.println("A? ");
